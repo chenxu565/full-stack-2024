a0314a83e09c8196ff96c50026358486170f6f84
Chen Xu <cxu@MacBook-Pro.local>
2024-02-09 22:26:43 +0200

    Go over part2

---
+++ part2/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+import noteService from './services/notes'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const hook = () => {
+    noteService
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
+      }
+    )
+  }
+  
+  useEffect(hook, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+  }
+
+  const addNote = event => {
+    event.preventDefault()
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    }
+
+    noteService
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+      }
+    )
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const notesToShow = showAll
+  ? notes
+  : notes.filter(note => note.important === true)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+            <Note key={note.id} note={note} toggleImportanceOf={toggleImportanceOf}/>
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input 
+          value={newNote}
+          onChange={handleNoteChange}  
+        />
+        <button type="submit">save</button>
+      </form>   
+    </div>
+  )
+}
+
+export default App 
---
+++ part2/src/components/Note.jsx
@@ -0,0 +1,15 @@
+const Note = ({ note, toggleImportanceOf }) => {
+    const label = note.important 
+      ? 'make not important' 
+      : 'make important';
+
+    return (
+  
+      <li className='note'>
+        {note.content} 
+        <button onClick={()=>toggleImportanceOf(note.id)}>{label}</button>
+      </li>
+    )
+  }
+
+export default Note
